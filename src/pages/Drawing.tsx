b984c3ee33c914613300dce822414b1641c2ea47
sondeokhyeon <sondeokhyeon@gmail.com>
2024-01-28 23:51:59 +0900

    Add : mouseDown시 좌표에따라 기본 사각형 생성 기능 추가

--- src/pages/Drawing.tsx
+++ src/pages/Drawing.tsx
@@ -1,31 +1,74 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "@/components/ui/Button";
 
 const Drawing = () => {
   const drawingElementRef = useRef<HTMLDivElement>(null);
+  const [shapeList, setShapeList] = useState<any[] | []>([]);
+  const currentShape = useRef<any | null>(null);
+  const shapeLists = useRef<any[] | []>([]);
+  const shapeRenderFlag = useRef(true);
+  const shapeRenderType = useRef<any | null>(null);
 
-  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
-    console.log(e);
-    const { clientX, clientY } = e;
-    console.log(clientX, clientY);
+  const handleMouseMove = (e: MouseEvent) => {};
+
+  const handleMouseUp = () => {};
+
+  const handleMouseDown = (e: MouseEvent) => {
+    const { offsetX, offsetY } = e;
+    setShapeList((prev) => {
+      const temp = {
+        left: offsetX,
+        top: offsetY,
+        width: 6,
+        height: 6,
+        index: prev.length,
+        translateX: 0,
+        translateY: 0,
+      };
+      currentShape.current = temp;
+      return [...prev, temp];
+    });
+    drawingElementRef.current?.addEventListener("mousemove", handleMouseMove);
+    drawingElementRef.current?.addEventListener("mouseup", handleMouseUp);
   };
 
-  const boxRenderHelper = () => {
-    drawingElementRef.current?.addEventListener("click", handleMouseDown);
+  const handleClear = () => setShapeList([]);
+
+  const shareRenderHelper = (shareType) => {
+    if (!drawingElementRef.current) return;
+    drawingElementRef.current?.addEventListener("mousedown", handleMouseDown);
   };
 
   return (
     <div className="flex justify-center items-center h-full w-full flex-col">
       <div className="w-[700px] h-[500px]">
         <div className="flex gap-x-[10px] items-start w-full">
-          <Button title="Box" onClick={boxRenderHelper} />
-          <Button title="Circle" onClick={() => console.log(4)} />
-          <Button title="Clear" onClick={() => console.log(4)} />
+          <Button title="Box" onClick={() => shareRenderHelper("box")} />
+          <Button title="Circle" onClick={() => shareRenderHelper("circle")} />
+          <Button title="Clear" onClick={handleClear} />
         </div>
         <div
-          className="border border-solid border-[#333] w-full h-full relative"
+          className="border border-solid border-[#333] w-full h-full relative z-[10]"
           ref={drawingElementRef}
-        ></div>
+        >
+          {shapeList.map((shape: any, idx) => {
+            return (
+              <div className="absolute left-0 top-0 w-full h-full">
+                <div
+                  key={idx}
+                  className="absolute border border-solid border-[#333]"
+                  data-type="shape"
+                  style={{
+                    left: shape?.left ?? 0,
+                    top: shape?.top ?? 0,
+                    width: shape?.width ?? 0,
+                    height: shape?.height ?? 0,
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
